ae2fcd8fda19ef26a689771c16aaa20208e7f729
Felix Pherry <123felixpherry@gmail.com>
2023-12-01 23:31:51 +0700

    Day 1

---
+++ README.md
@@ -0,0 +1,29 @@
+# Advent of Code
+
+## Overview
+
+Welcome to my repository containing solutions for Advent of Code 2023, implemented in TypeScript
+
+## Getting Started
+
+### Clone or Download
+
+Clone or download this repository to your local machine.
+
+### Install dependencies
+
+```bash
+pnpm install
+```
+
+### Running the Script
+
+Replace day1 with the corresponding day number for which you want to run the script.
+
+```bash
+pnpm run day1
+```
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details. You are free to modify and distribute the adapted code as needed for your own projects.
---
+++ src/01/index.ts
@@ -0,0 +1,132 @@
+import * as fs from 'fs';
+
+const isNum = (ch: string) => {
+  return ch[0] <= '9' && ch[0] >= '0';
+};
+
+const part1 = () => {
+  try {
+    const data = fs.readFileSync('./input.txt', 'utf-8');
+    const items = data.split('\n');
+    let sum = items.reduce((tot, item) => {
+      let first: number | null = null,
+        second: number | null = null;
+      for (const ch of item) {
+        if (isNum(ch)) {
+          if (first === null) first = parseInt(ch);
+          second = parseInt(ch);
+        }
+      }
+      if (!first || !second) {
+        console.log({ item, first, second });
+        throw new Error('Data is invalid');
+      }
+      return tot + 10 * first + second;
+    }, 0);
+    console.log('Part 1', sum);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const part2 = () => {
+  try {
+    const data = fs.readFileSync('./input.txt', 'utf-8');
+    const items = data.split('\n');
+    let sum = items.reduce((tot, item) => {
+      let first: number | null = null,
+        second: number | null = null;
+      for (let i = 0; i < item.length; i++) {
+        if (isNum(item[i])) {
+          if (first === null) first = parseInt(item[i]);
+          second = parseInt(item[i]);
+        } else if (
+          item[i] === 'o' &&
+          item[i + 1] === 'n' &&
+          item[i + 2] === 'e'
+        ) {
+          if (first === null) first = 1;
+          second = 1;
+        } else if (
+          item[i] === 't' &&
+          item[i + 1] === 'w' &&
+          item[i + 2] === 'o'
+        ) {
+          if (first === null) first = 2;
+          second = 2;
+        } else if (
+          item[i] === 't' &&
+          item[i + 1] === 'h' &&
+          item[i + 2] === 'r' &&
+          item[i + 3] === 'e' &&
+          item[i + 4] === 'e'
+        ) {
+          if (first === null) first = 3;
+          second = 3;
+        } else if (
+          item[i] === 'f' &&
+          item[i + 1] === 'o' &&
+          item[i + 2] === 'u' &&
+          item[i + 3] === 'r'
+        ) {
+          if (first === null) first = 4;
+          second = 4;
+        } else if (
+          item[i] === 'f' &&
+          item[i + 1] === 'i' &&
+          item[i + 2] === 'v' &&
+          item[i + 3] === 'e'
+        ) {
+          if (first === null) first = 5;
+          second = 5;
+        } else if (
+          item[i] === 's' &&
+          item[i + 1] === 'i' &&
+          item[i + 2] === 'x'
+        ) {
+          if (first === null) first = 6;
+          second = 6;
+        } else if (
+          item[i] === 's' &&
+          item[i + 1] === 'e' &&
+          item[i + 2] === 'v' &&
+          item[i + 3] === 'e' &&
+          item[i + 4] === 'n'
+        ) {
+          if (first === null) first = 7;
+          second = 7;
+        } else if (
+          item[i] === 'e' &&
+          item[i + 1] === 'i' &&
+          item[i + 2] === 'g' &&
+          item[i + 3] === 'h' &&
+          item[i + 4] === 't'
+        ) {
+          if (first === null) first = 8;
+          second = 8;
+        } else if (
+          item[i] === 'n' &&
+          item[i + 1] === 'i' &&
+          item[i + 2] === 'n' &&
+          item[i + 3] === 'e'
+        ) {
+          if (first === null) first = 9;
+          second = 9;
+        }
+      }
+      if (!first || !second) {
+        console.log({ item, first, second });
+        throw new Error('Data is invalid');
+      }
+      return tot + 10 * first + second;
+    }, 0);
+    console.log('Part 2', sum);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Time Complexity for both: O(row * col)
+// Space Complexity for both: O(1)
+part1();
+part2();
